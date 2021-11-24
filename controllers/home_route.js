82ce8445c832039b5f97b6678fa870a948576c74
Jose Avalos <avaljose@hotmai.com>
2021-11-23 20:06:19 -0600

    working on handlebars

--- controllers/home_route.js
+++ controllers/home_route.js
@@ -1,48 +1,47 @@
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const router = require('express').Router();
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-    console.log(req.session);
-  
-    Post.findAll({
-      attributes: ["id", "title", "created_at", "post_content"],
-      include: [
-        {
-          model: Comment,
-          attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-          include: {
-            model: User,
-            attributes: ["username"],
-          },
-        },
-        {
-          model: User,
-          attributes: ["username"],
-        },
-      ],
-    })
-      .then((dbPostData) => {
-        const posts = dbPostData.map((post) => post.get({ plain: true }));
-        res.render("homepage", {
-          posts,
-          loggedIn: req.session.loggedIn,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+  console.log(req.session);
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
+  Post.findAll({
+    attributes: ["id", "title", "created_at", "content"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["name"],
+      },
+    ],
+  })
+    .then((postData) => {
+      const posts = postData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts
+        // loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
+  res.render("login");
+});
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -4,13 +4,6 @@ const userSeed = require('./user_seed.js');
 const commentSeed = require('./comment_seed.js');
 const postSeed = require('./post_seed.js');
 
-process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD
-
-console.log("usernmae",process.env.DB_NAME)
-console.log("DB_USER",process.env.DB_USER)
-console.log("DB_PASSWORD",process.env.DB_PASSWORD)
-
-
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
   await userSeed();
