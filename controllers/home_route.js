62fc73c3e4d1b2d6f569da080611cb786cde37eb
Jose Avalos <avaljose@hotmai.com>
2021-11-23 22:17:40 -0600

    working on users routes

--- controllers/home_route.js
+++ controllers/home_route.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
-
+// get posts /direct to homepage
 router.get("/", (req, res) => {
   console.log(req.session);
 
@@ -25,8 +25,8 @@ router.get("/", (req, res) => {
     .then((postData) => {
       const posts = postData.map((post) => post.get({ plain: true }));
       res.render("homepage", {
-        posts
-        // loggedIn: req.session.loggedIn,
+        posts,
+        loggedIn: req.session.loggedIn,
       });
     })
     .catch((err) => {
@@ -34,7 +34,7 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
-
+ //login route
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
@@ -44,4 +44,54 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "created_at", "post_content"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // make sure data pass to template here
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 module.exports = router;
---
+++ public/js/signup.js
@@ -0,0 +1,31 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector("#name_signup").value.trim();
+    const email = document.querySelector("#email_signup").value.trim();
+    const password = document.querySelector("#password_signup").value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch("/api/users", {
+        method: "post",
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      // check the response status
+      if (response.ok) {
+        console.log("success");
+        document.location.replace("/dashboard");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document
+    .querySelector(".signup_form")
+    .addEventListener("submit", signupFormHandler);
